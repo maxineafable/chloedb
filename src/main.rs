6312113fac1250a082a64df134afc07a51755f17
maxineafable <maxineafable@yahoo.com>
2026-07-29 20:58:47 +0800

    fix: load hashmap from json file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::{Parser, Subcommand};
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::BufWriter;
+use std::io::{BufReader, BufWriter};
 
 #[derive(Parser)]
 struct Args {
@@ -16,28 +16,44 @@ enum Commands {
     Remove { key: String },
 }
 
+fn save_to_json(
+    filename: &str,
+    db: &HashMap<String, String>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::create(filename)?;
+    let writer = BufWriter::new(file);
+
+    serde_json::to_writer_pretty(writer, &db)?;
+
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
+    let filename = "map.json";
 
-    let mut db: HashMap<String, String> = HashMap::new();
+    let mut db: HashMap<String, String> = match File::open(filename) {
+        Ok(file) => serde_json::from_reader(BufReader::new(file))?,
+        Err(error) => match error.kind() {
+            std::io::ErrorKind::NotFound => HashMap::new(),
+            _ => return Err(error.into()),
+        },
+    };
 
     match args.command {
         Commands::Set { key, value } => {
             db.insert(key, value);
+            save_to_json(filename, &db)?;
         }
         Commands::Get { key } => match db.get(&key) {
             Some(value) => println!("{}", value),
-            None => println!("Key not found"),
+            None => println!("key: \"{}\" not found", key),
         },
         Commands::Remove { key } => {
             db.remove(&key);
+            save_to_json(filename, &db)?;
         }
     }
-
-    let file = File::create("map.json")?;
-    let writer = BufWriter::new(file);
-
-    serde_json::to_writer_pretty(writer, &db)?;
 
     Ok(())
 }
