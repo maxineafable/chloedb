24c0e86f2b8e4ae43a5fc67633ca4cc560a53f7a
maxineafable <maxineafable@yahoo.com>
2026-07-30 08:47:50 +0800

    feat: replace json file with append-only log

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use clap::{Parser, Subcommand};
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufReader, BufWriter};
+use std::fs::OpenOptions;
+use std::io::BufRead;
+use std::io::Write;
+use std::io::{BufReader};
+use std::str::FromStr;
+use strum::EnumString;
 
 #[derive(Parser)]
 struct Args {
@@ -16,42 +21,52 @@ enum Commands {
     Remove { key: String },
 }
 
-fn save_to_json(
-    filename: &str,
-    db: &HashMap<String, String>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let file = File::create(filename)?;
-    let writer = BufWriter::new(file);
+#[derive(EnumString)]
+#[strum(serialize_all = "UPPERCASE")]
+enum Command {
+    Set,
+    Remove,
+}
 
-    serde_json::to_writer_pretty(writer, &db)?;
-
-    Ok(())
+fn open_log() -> Result<File, std::io::Error> {
+    OpenOptions::new()
+        .read(true)
+        .create(true)
+        .append(true)
+        .open("log.txt")
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    let filename = "map.json";
 
-    let mut db: HashMap<String, String> = match File::open(filename) {
-        Ok(file) => serde_json::from_reader(BufReader::new(file))?,
-        Err(error) => match error.kind() {
-            std::io::ErrorKind::NotFound => HashMap::new(),
-            _ => return Err(error.into()),
-        },
-    };
+    let mut log = open_log()?;
+
+    let reader = BufReader::new(&log);
+    let mut map: HashMap<String, String> = HashMap::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        let first = parts[0];
+        let command = Command::from_str(first)?;
+        match command {
+            Command::Set => map.insert(parts[1].to_string(), parts[2].to_string()),
+            Command::Remove => map.remove(parts[1]),
+        };
+    }
 
     match args.command {
         Commands::Set { key, value } => {
-            db.insert(key, value);
-            save_to_json(filename, &db)?;
+            let command = format!("SET {} {}", key, value);
+            writeln!(log, "{}", command)?;
         }
-        Commands::Get { key } => match db.get(&key) {
+        Commands::Get { key } => match map.get(&key) {
             Some(value) => println!("{}", value),
             None => println!("key: \"{}\" not found", key),
         },
         Commands::Remove { key } => {
-            db.remove(&key);
-            save_to_json(filename, &db)?;
+            let command = format!("REMOVE {}", key);
+            writeln!(log, "{}", command)?;
         }
     }
 
